79136e8acbd79eff0f36f602a8661d6d0b180a01
Dawid Łazuk <dawid.lazuk@itmagination.com>
2018-11-14 18:51:29 +0100

    zajecia

--- bandit.py
+++ bandit.py
@@ -6,10 +6,16 @@ Created on Tue Nov 13 20:30:30 2018
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 #n_arms = 2
+LinearFactor = 0.01
 
+RewardProbability = 0.5
 
+def linear_reward(reward):
+    return lambda it: LinearFactor * it + reward
 
 class Arm:
     def __init__(self, actions):
@@ -26,13 +32,15 @@ class Action:
         self.reward_f = reward_f     
         
     def pull(self, pulled_times):
-        return self.reward_f(pulled_times)      
-        
+        if(np.random.random() < RewardProbability):      
+            return self.reward_f(pulled_times)    
+        return 0
+            
 class Bandit:
     def __init__(self, n_actions_per_arm):
         self.arms = [
-                Arm(actions = [Action(lambda it: 0.1), Action(lambda it: 0.8)]),
-                Arm(actions = [Action(lambda it: 0.9), Action(lambda it: 0.2)])
+                Arm(actions = [Action(linear_reward(0.1)), Action(linear_reward(0.8))]),
+                Arm(actions = [Action(linear_reward(0.9)), Action(linear_reward(0.2))])
             ];
         self.actions_prob = np.array([0.5,0.5])
         
@@ -79,7 +87,8 @@ class Player_WithoutCaseKnowledge:
         
     def perform_step(self):
         self.steps_performed = self.steps_performed + 1
-        self.epsilon =  100 / self.steps_performed
+        self.epsilon =  math.pow((StepsNumber - self.steps_performed) / StepsNumber,3)
+        
         A = self.get_next_arm()
         R = bandit.pull_arm(A)
         self.N[A] = self.N[A] + 1
@@ -110,13 +119,13 @@ class Player_WithCaseKnowledge:
             #explore
             print('Explore')
             #tutaj można wybierać te akcje, które mało razy zostały przeszukane
-            return np.argmin(self.N[:,bandit.current_action])
+            #return np.argmin(self.N[:,bandit.current_action])
             #losowo (według algorytmu)
-            #return np.random.randint(0,bandit.n_arms)            
+            return np.random.randint(0,bandit.n_arms)            
         
     def perform_step(self):
         self.steps_performed = self.steps_performed + 1
-        self.epsilon =  100 / self.steps_performed
+        self.epsilon =  math.pow((StepsNumber - self.steps_performed) / StepsNumber,3)
 
         curr_action = bandit.current_action
         A = self.get_next_arm()
@@ -126,14 +135,21 @@ class Player_WithCaseKnowledge:
         
         print('Q ', self.Q, 'N ', self.N, ' epsilon ', self.epsilon)
         return R
-              
+             
+StepsNumber = 1000
     
 bandit = Bandit(2)          
 player = Player_WithCaseKnowledge(bandit)
 
-for i in range(10000):
+plotdata = []
+
+for i in range(StepsNumber):
     reward = player.perform_step();
+    plotdata.append([reward])
     print('Step ', i, ' reward ', reward)
+    
+plt.figure(figsize=(15,10))
+plt.plot(plotdata)
     
     
     
