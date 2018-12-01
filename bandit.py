485f37ffa5bef7d302b91a524785c4358b7a6700
Dawid Lazuk <dawid.lazuk@gmail.com>
2018-12-01 13:55:21 +0100

    Drukowanie wart. funkcji akcji i skrypty testowe

--- bandit.py
+++ bandit.py
@@ -5,8 +5,8 @@ import math
 ###### Implementacja ######
 
 #Liniowa funkcja wartosci akcji
-def linear_reward(lin_factor):
-    return lambda step: lin_factor * step
+def linear_reward(a, b):
+    return lambda step: a * step + b
 
 #Pojedyncze ramie maszyny
 class Arm:
@@ -149,6 +149,26 @@ def printPlot_dots(subplot, array, title, x_label, y_label):
     subplot.set_title(title)
     subplot.set_xlabel(x_label)
     subplot.set_ylabel(y_label)
+    
+def print_actionstatevalue_subplot(subplot, array):
+    subplot.plot(array)
+    
+def get_actionstatevalue_per_steps(Q_values, action, state):
+    return Q_values[:,action, state];
+
+def print_Q_plot(Q_values):
+    Q_values = np.array(Q_values)
+    while(Q_values.ndim < 3):
+        Q_values = np.expand_dims(Q_values, axis=2)
+    fig, axs = plt.subplots(1, 1, constrained_layout=True)
+    legend = []
+    for state in range(Q_values.shape[2]):
+        for action in range(Q_values.shape[1]):
+            actionStateValue_per_steps = get_actionstatevalue_per_steps(Q_values, action, state)
+            printPlot_lines(axs, actionStateValue_per_steps, 'Funkcja akcji w danym kroku', 'Krok', 'Wartosc')
+            legend.append(f'Stan: {state}, akcja: {action}')
+    plt.legend(legend)
+    plt.show()
 
 #Pobieranie danych danego klastra z tablicy
 def GetBatch(array, index):
@@ -157,62 +177,195 @@ def GetBatch(array, index):
 
 ###### Eksperymenty ######
 
-#Inicjalizacja stałych
-LinearFactor = 0.01
-StepsNumber = 20000
-Batch_size = 100
-Bandit_array = np.array([[0.1, 0.4 , 0.1], [0.3, 0.2, 0.1]])
 
-Batch_number = (int)(StepsNumber/Batch_size)
+def PerformAnExperiment(player_type, bandit_array, reward_fun, steps_number, batch_size):    
+    Batch_number = (int)(steps_number/batch_size)
     
-#Inicjalizacja maszyny
-bandit = Bandit(Bandit_array, linear_reward(LinearFactor))          
-
-#Inicjalizacja gracza (wybór klasy jako algorytmu)
-player = Player_WithoutCaseKnowledge(bandit)
-
-#Listy na dane do wykresów
-rewards_per_step = []
-epsilon_on_step = []
-
-#Wykonaj iteracje algorytmu
-for i in range(StepsNumber):
-    reward, curr_eps = player.perform_step();
-    rewards_per_step.append([reward])        
-    epsilon_on_step.append([curr_eps])
-    print('Step ', i, ' reward ', reward)
-
-#Zlicz sumę zebranych nagród dla każdego kroku
-total_reward_per_step = np.cumsum(rewards_per_step)
-
-#Wyznaczenie sumy nagród zdobytych w klastrze
-reward_value_in_batch = []
-for i in range(Batch_number):
-    value_in_batch = np.sum(rewards_per_step[i*Batch_size : (i+1)*Batch_size-1])
-    reward_value_in_batch.append(value_in_batch)
-
-#Wyznaczenie ilosci nagród zdobytych w klastrze    
-reward_number_in_batch = []
-for i in range(Batch_number):
-    number_in_batch = np.count_nonzero(GetBatch(rewards_per_step, i))
-    reward_number_in_batch.append(number_in_batch)      
+    #Inicjalizacja maszyny
+    bandit = Bandit(bandit_array, reward_fun)          
     
-#Wykres nagród w krokach    
-fig, axs = plt.subplots(2, 1, constrained_layout=True)
-printPlot_dots(axs[0], rewards_per_step, 'Nagroda w każdym kroku', 'Kroki', 'Wartosc nagrody')
-printPlot_lines(axs[1], total_reward_per_step, 'Suma nagród w każdym kroku', 'Kroki', 'Suma nagrody')
-fig.set_size_inches(10,10)
-plt.show()
+    #Inicjalizacja gracza (wybór klasy jako algorytmu)
+    player = player_type(bandit)
+    
+    #Listy na dane do wykresów
+    rewards_per_step = []
+    epsilon_on_step = []
+    Q_values_per_step = []
+    
+    #Wykonaj iteracje algorytmu
+    for i in range(steps_number):
+        reward, curr_eps = player.perform_step();
+        rewards_per_step.append([reward])        
+        epsilon_on_step.append([curr_eps])
+        Q_values_per_step.append(np.copy(player.Q))
+        print('Step ', i, ' reward ', reward)
+    
+    #Zlicz sumę zebranych nagród dla każdego kroku
+    total_reward_per_step = np.cumsum(rewards_per_step)
+    
+    #Wyznaczenie sumy nagród zdobytych w klastrze
+    reward_value_in_batch = []
+    for i in range(Batch_number):
+        value_in_batch = np.sum(rewards_per_step[i*batch_size : (i+1)*batch_size-1])
+        reward_value_in_batch.append(value_in_batch)
+    
+    #Wyznaczenie ilosci nagród zdobytych w klastrze    
+    reward_number_in_batch = []
+    for i in range(Batch_number):
+        number_in_batch = np.count_nonzero(GetBatch(rewards_per_step, i))
+        reward_number_in_batch.append(number_in_batch)      
+        
+    #Wykres nagród w krokach    
+    fig, axs = plt.subplots(2, 1, constrained_layout=True)
+    printPlot_dots(axs[0], rewards_per_step, 'Nagroda w każdym kroku', 'Kroki', 'Wartosc nagrody')
+    printPlot_lines(axs[1], total_reward_per_step, 'Suma nagród w każdym kroku', 'Kroki', 'Suma nagrody')
+    fig.set_size_inches(10,10)
+    plt.show()
+    
+    #Wykres nagród w klastrach
+    fig, axs = plt.subplots(2, 1, constrained_layout=True)
+    printPlot_dots(axs[0], reward_value_in_batch, 'Wartosć nagród w klastrach', 'Klastry', 'Wartosc nagrody')
+    printPlot_lines(axs[1], reward_number_in_batch, 'Ilosc sukcesow w klastrach', 'Klastry', 'Ilosc sukcesów')
+    fig.set_size_inches(10,10)
+    plt.show()
+    
+    #wykres epsilon
+    fig, axs = plt.subplots(1, 1, constrained_layout=True)
+    printPlot_lines(axs, epsilon_on_step, 'Wartosć epsilon w każdym kroku', 'Kroki', 'Wartosć epsilon')
+    fig.set_size_inches(10,5)
+    plt.show()
+    
+    print_Q_plot(Q_values_per_step)
+    
+def Case_Player_WithoutKnowledge_ConstFun():
+    LinearFactor = 0.0
+    LinearStartValue = 0.1
+    Steps_Number = 10000
+    Batch_size = 100
+    Bandit_array = np.array([[0.1, 0.4], [0.3, 0.2]])
+    
+    player_type = Player_WithoutCaseKnowledge
+    reward_fun = linear_reward(LinearFactor, LinearStartValue)
+        
+    PerformAnExperiment(player_type,
+                        Bandit_array,
+                        reward_fun,
+                        Steps_Number,
+                        Batch_size)
+    
+def Case_Player_WithoutKnowledge_LinearFun():
+    LinearFactor = 0.1
+    LinearStartValue = 0.01
+    Steps_Number = 10000
+    Batch_size = 100
+    Bandit_array = np.array([[0.1, 0.4], [0.3, 0.2]])
+    
+    player_type = Player_WithoutCaseKnowledge
+    reward_fun = linear_reward(LinearFactor, LinearStartValue)
+        
+    PerformAnExperiment(player_type,
+                        Bandit_array,
+                        reward_fun,
+                        Steps_Number,
+                        Batch_size)
+    
+def Case_Player_WithKnowledge_ConstFun():
+    LinearFactor = 0.0
+    LinearStartValue = 0.1
+    Steps_Number = 10000
+    Batch_size = 100
+    Bandit_array = np.array([[0.1, 0.4], [0.3, 0.2]])
+    
+    player_type = Player_WithCaseKnowledge
+    reward_fun = linear_reward(LinearFactor, LinearStartValue)
+        
+    PerformAnExperiment(player_type,
+                        Bandit_array,
+                        reward_fun,
+                        Steps_Number,
+                        Batch_size)
+    
+def Case_Player_WithKnowledge_LinearFun():
+    LinearFactor = 0.1
+    LinearStartValue = 0.1
+    Steps_Number = 10000
+    Batch_size = 100
+    Bandit_array = np.array([[0.1, 0.4], [0.3, 0.2]])
+    
+    player_type = Player_WithCaseKnowledge
+    reward_fun = linear_reward(LinearFactor, LinearStartValue)
+        
+    PerformAnExperiment(player_type,
+                        Bandit_array,
+                        reward_fun,
+                        Steps_Number,
+                        Batch_size)
 
-#Wykres nagród w klastrach
-fig, axs = plt.subplots(2, 1, constrained_layout=True)
-printPlot_dots(axs[0], reward_value_in_batch, 'Wartosć nagród w klastrach', 'Klastry', 'Wartosc nagrody')
-printPlot_lines(axs[1], reward_number_in_batch, 'Ilosc sukcesow w klastrach', 'Klastry', 'Ilosc sukcesów')
-fig.set_size_inches(10,10)
-plt.show()
-
-#wykres epsilon
-fig, axs = plt.subplots(1, 1, constrained_layout=True)
-printPlot_lines(axs, epsilon_on_step, 'Wartosć epsilon w każdym kroku', 'Kroki', 'Wartosć epsilon')
-fig.set_size_inches(10,5)
-plt.show()
+        
+def obsolete():    
+    #Inicjalizacja stałych
+    LinearFactor = 0.0
+    LinearStartValue = 1
+    StepsNumber = 10000
+    Batch_size = 100
+    Bandit_array = np.array([[0.1, 0.4], [0.3, 0.2]])
+    
+    Batch_number = (int)(StepsNumber/Batch_size)
+        
+    #Inicjalizacja maszyny
+    bandit = Bandit(Bandit_array, linear_reward(LinearFactor, LinearStartValue))          
+    
+    #Inicjalizacja gracza (wybór klasy jako algorytmu)
+    player = Player_WithoutCaseKnowledge(bandit)
+    
+    #Listy na dane do wykresów
+    rewards_per_step = []
+    epsilon_on_step = []
+    Q_values_per_step = []
+    
+    #Wykonaj iteracje algorytmu
+    for i in range(StepsNumber):
+        reward, curr_eps = player.perform_step();
+        rewards_per_step.append([reward])        
+        epsilon_on_step.append([curr_eps])
+        Q_values_per_step.append(np.copy(player.Q))
+        print('Step ', i, ' reward ', reward)
+    
+    #Zlicz sumę zebranych nagród dla każdego kroku
+    total_reward_per_step = np.cumsum(rewards_per_step)
+    
+    #Wyznaczenie sumy nagród zdobytych w klastrze
+    reward_value_in_batch = []
+    for i in range(Batch_number):
+        value_in_batch = np.sum(rewards_per_step[i*Batch_size : (i+1)*Batch_size-1])
+        reward_value_in_batch.append(value_in_batch)
+    
+    #Wyznaczenie ilosci nagród zdobytych w klastrze    
+    reward_number_in_batch = []
+    for i in range(Batch_number):
+        number_in_batch = np.count_nonzero(GetBatch(rewards_per_step, i))
+        reward_number_in_batch.append(number_in_batch)      
+        
+    #Wykres nagród w krokach    
+    fig, axs = plt.subplots(2, 1, constrained_layout=True)
+    printPlot_dots(axs[0], rewards_per_step, 'Nagroda w każdym kroku', 'Kroki', 'Wartosc nagrody')
+    printPlot_lines(axs[1], total_reward_per_step, 'Suma nagród w każdym kroku', 'Kroki', 'Suma nagrody')
+    fig.set_size_inches(10,10)
+    plt.show()
+    
+    #Wykres nagród w klastrach
+    fig, axs = plt.subplots(2, 1, constrained_layout=True)
+    printPlot_dots(axs[0], reward_value_in_batch, 'Wartosć nagród w klastrach', 'Klastry', 'Wartosc nagrody')
+    printPlot_lines(axs[1], reward_number_in_batch, 'Ilosc sukcesow w klastrach', 'Klastry', 'Ilosc sukcesów')
+    fig.set_size_inches(10,10)
+    plt.show()
+    
+    #wykres epsilon
+    fig, axs = plt.subplots(1, 1, constrained_layout=True)
+    printPlot_lines(axs, epsilon_on_step, 'Wartosć epsilon w każdym kroku', 'Kroki', 'Wartosć epsilon')
+    fig.set_size_inches(10,5)
+    plt.show()
+    
+    print_Q_plot(Q_values_per_step)
+    
+        
